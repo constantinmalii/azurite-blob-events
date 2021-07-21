dd833be511adf6cdbdf07c0fb0da2b50945ac233
Constantin Malii <constantin@symend.com>
2021-07-21 11:40:07 -0600

    Added blob trigger function.

---
+++ RabbitEventHubTest.FunctionApp/BlobTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace RabbitEventHubTest.FunctionApp
+{
+    public static class BlobTrigger
+    {
+        [FunctionName("BlobTrigger")]
+        public static async Task RunAsync([BlobTrigger("azurite/azurite-test", Connection = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;")]
+            Stream myBlob, string name, ILogger log)
+        {
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+            
+        }
+    }
+}
